8f107d278e713bce0f185a522c3975c2c6643193
Juan Carlos <mrfpgapro@gmail.com>
2022-01-19 13:05:42 -0800

    01/19/2022  -  1:05 PM

---
+++ global.js
@@ -0,0 +1,9 @@
+
+
+
+// Need "export" to be able to export variables and functions as global variables
+// "let" is the same as "var" but "var" is slowly being phased out in favor of "let"
+export let my_name = "John Doe";
+
+
+
--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-
+// Dependencies
 import { fileTypeFromFile } from 'file-type';
 
+// My local includes
+import * as global from "./global.js"
 
-console.log(await fileTypeFromFile('imperial_march_noisy.wav'));
+// View the file type using "file-type"
+//console.log(await fileTypeFromFile('imperial_march_noisy.wav'));
 
 
+console.log(global.my_name);
 
 
