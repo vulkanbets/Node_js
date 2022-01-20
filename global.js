bad2d954438be13d33a680bf285ec5bd18b43f9f
Juan Carlos <mrfpgapro@gmail.com>
2022-01-19 16:05:26 -0800

    01/19/2022  -  4:05 PM

--- global.js
+++ global.js
@@ -1,9 +1,12 @@
 
 
-
 // Need "export" to be able to export variables and functions as global variables
 // "let" is the same as "var" but "var" is slowly being phased out in favor of "let"
 export let my_name = "John Doe";
+
+
+
+
 
 
 
