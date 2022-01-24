2c9ecff52436a43628233e4cc05a5f29d78f1b27
Juan Carlos <mrfpgapro@gmail.com>
2022-01-24 01:48:41 -0800

    1:48 AM  -  01/24/2022

--- global.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.my_name = void 0;
-// Need "export" to be able to export variables and functions as global variables
-// "let" is the same as "var" but "var" is slowly being phased out in favor of "let"
-exports.my_name = "John Doe";
