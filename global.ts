e61e1936d367e180bddf359675c0b763f2eeba69
Juan Carlos <mrfpgapro@gmail.com>
2022-01-24 01:37:36 -0800

    1:37 AM  -  01/24/2022

--- global.ts
+++ global.ts
@@ -1,6 +1,11 @@
-"use strict";
-exports.__esModule = true;
-exports.my_name = void 0;
+
 // Need "export" to be able to export variables and functions as global variables
 // "let" is the same as "var" but "var" is slowly being phased out in favor of "let"
-exports.my_name = "John Doe";
+export let my_name = "John Doe";
+
+
+
+
+
+
+
---
+++ main.ts
@@ -0,0 +1,38 @@
+// Dependencies
+// import { fileTypeFromFile } from 'file-type';
+
+// My local includes
+import * as global from "./global"
+
+
+// View the file type using "file-type"
+// console.log(await fileTypeFromFile('imperial_march_noisy.wav'));
+
+
+// This is how you call global varaibles
+console.log(global.my_name);
+
+
+
+
+// js pointer example
+//const obj1 =
+//{
+//    city: 'Boston',
+//    state: 'Massachusetts'
+//};
+
+//const obj2 = obj1;
+//obj2.city = 'Springfield';
+
+//console.log(obj1); // {city: 'Springfield', state: 'Massachusetts'};
+
+
+
+
+
+
+
+
+
+
